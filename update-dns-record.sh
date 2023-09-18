7d9b099f3b1f4431a966c5b22c8691a1f4b82a1a
sushma-b23 <susmaboothu5@gmail.com>
2023-09-18 17:02:17 +0530

    jenkins

--- update-dns-record.sh
+++ update-dns-record.sh
@@ -1,6 +1,6 @@
 Ip=$(aws ec2 describe-instances --filters "Name=tag:Name, Values=jenkins" --query 'Reservations[*].Instances[*].PublicIpAddress --output text')
 
-echo
+echo '
 {
   "Comment": "CREATE/DELETE/UPSERT  a record",
   "Changes": [{
@@ -11,4 +11,7 @@ echo
       "ResourceRecords": [{"value": "IPADDRESS"}]
     }
   }]
-} | sed -e "s/IPADDRESS|${IP}/"
+} | sed -e "s/IPADDRESS|${IP}/" >/tmp/jenkins.json
+
+ZONE_ID="Z03482602CK395N17NV00"
+aws rout53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/recordjenkins.json | jq .
